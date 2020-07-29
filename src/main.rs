6502e82162ead953de98f69eab201dc15eb9c895
Aaron Kofsky <aaronko@umich.edu>
2020-07-29 14:54:18 -0400

    RAYON_MULTITHREADING
    Add rayon, a multithreading crate. We use rayon to parallelize the
    bytebeat rendering code, achieving about a 6x speedup and making 1024x1024
    resolution real time feasible.
    
    Also, make rand use a specific version instead of * (the project was started
    like a billion years ago so it uses a super old version of rand)
    
    Oh also make input handling a bit nicer to read (created a "Controls" enum
    that lets me not parse individual events)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use evobf::bf;
 use evobf::bytebeat;
 
-extern crate pixel_canvas;
-extern crate rand;
-
 use pixel_canvas::{
     input::{
         glutin::event::{ElementState, KeyboardInput, MouseButton, VirtualKeyCode},
@@ -12,16 +9,18 @@ use pixel_canvas::{
     Canvas, Color, Image,
 };
 use rand::Rng;
+use rayon::prelude::*;
+
 // the actual size, in pixels of the window to display
-const WINDOW_WIDTH: usize = 512;
-const WINDOW_HEIGHT: usize = 512;
+const WINDOW_WIDTH: usize = 1024;
+const WINDOW_HEIGHT: usize = 1024;
 // the internal size, in "pixels" of the bytebeat to render
-const BYTEBEAT_WIDTH: usize = 512;
-const BYTEBEAT_HEIGHT: usize = 512;
+const BYTEBEAT_WIDTH: usize = 1024;
+const BYTEBEAT_HEIGHT: usize = 1024;
 // the size of pixels for a brainfuck program
 const PIXEL_SIZE: usize = 32;
 const INITIAL_SPEED: usize = 500;
-const PROGRAM_LENGTH: usize = 100;
+const PROGRAM_LENGTH: usize = 20;
 fn main() {
     println!("BROKEN_FIELD_START");
     let canvas = Canvas::new(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -31,90 +30,31 @@ fn main() {
             pixel_canvas::input::MouseState::handle_input(info, &mut state.mouse, event);
             // println!("new event {:?}", event);
             match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::MouseInput {
-                        button: MouseButton::Left,
-                        state: ElementState::Pressed,
-                        ..
-                    } => {
-                        *state = State::new();
-                    }
-                    WindowEvent::MouseInput {
-                        button: MouseButton::Right,
-                        state: ElementState::Pressed,
-                        ..
-                    } => {
-                        // restart the program without changing it
-                        state.state = bf::BFState::new();
-                        state.frame = 0;
-                    }
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(keycode),
-                                ..
-                            },
-                        ..
-                    } => {
-                        match keycode {
-                            VirtualKeyCode::Right => {
-                                state.bytebeat_speed += 1;
-                                state.brainfuck_speed += 1;
-                            }
-                            VirtualKeyCode::Left => {
-                                state.bytebeat_speed -= 1;
-                                state.brainfuck_speed = state.brainfuck_speed.saturating_sub(1);
-                            }
-                            VirtualKeyCode::Up => {
-                                state.bytebeat_speed *= 2;
-                                // cap is here so that the program doesnt hang if you set the speed to like, 4 billion
-                                state.brainfuck_speed = (state.brainfuck_speed * 2).min(200_000);
-                            }
-                            VirtualKeyCode::Down => {
-                                state.bytebeat_speed /= 2;
-                                state.brainfuck_speed /= 2;
-                            }
-                            _ => (),
-                        };
-
-                        match keycode {
-                            VirtualKeyCode::Right
-                            | VirtualKeyCode::Left
-                            | VirtualKeyCode::Up
-                            | VirtualKeyCode::Down => println!(
-                                "Bytebeat Speed: {} (t = {})",
-                                state.bytebeat_speed, state.frame
+                Event::WindowEvent { event, .. } => {
+                    if let Some(control) = Controls::from_event(event) {
+                        state.bytebeat.handle_input(control);
+                        match control {
+                            Controls::Faster
+                            | Controls::VeryFaster
+                            | Controls::Slower
+                            | Controls::VerySlower => println!(
+                                "Speed = {} (t = {})",
+                                state.bytebeat.speed, state.bytebeat.frame
                             ),
                             _ => (),
-                        };
+                        }
                     }
-                    _ => (),
-                },
+                }
                 _ => (),
-            };
+            }
             true
         });
 
     canvas.render(|state, image| {
-        for screen_y in 0..BYTEBEAT_WIDTH {
-            for screen_x in 0..BYTEBEAT_HEIGHT {
-                let idx = screen_y * BYTEBEAT_WIDTH + screen_x;
-                state.bytebeat_data[idx] = bytebeat::eval_beat(
-                    &mut state.stack,
-                    &state.bytebeat,
-                    state.frame,
-                    state.mouse.x as i64,
-                    state.mouse.y as i64,
-                    screen_x as i64,
-                    screen_y as i64,
-                )
-                .into();
-            }
-        }
-
-        render_image(image, state.bytebeat_data.as_ref());
-        state.frame += state.bytebeat_speed;
+        // let start = std::time::Instant::now();
+        state.bytebeat.render(image, &state.mouse);
+        state.bytebeat.frame += state.bytebeat.speed;
+        // println!("Time: {:?}", start.elapsed());
         // for _ in 0..state.index {
         //     if !halted(&state.state, &state.program) {
         //         state.state.step(&state.program, &mut state.input);
@@ -133,43 +73,209 @@ fn main() {
         // );
     });
 }
-pub struct State {
-    pub stack: Vec<bytebeat::Val>,
-    pub bytebeat: bytebeat::Program,
-    pub program: bf::Program,
-    pub state: bf::BFState,
-    pub bytebeat_speed: i64,
-    pub brainfuck_speed: usize,
+
+#[derive(Debug, Copy, Clone)]
+enum Controls {
+    New,
+    Restart,
+    Next,
+    Prev,
+    Mutate,
+    VerySlower,
+    Slower,
+    Faster,
+    VeryFaster,
+}
+
+impl Controls {
+    fn from_event(event: &WindowEvent) -> Option<Controls> {
+        use Controls::*;
+        use VirtualKeyCode::*;
+        match event {
+            WindowEvent::MouseInput {
+                button: MouseButton::Left,
+                state: ElementState::Released,
+                ..
+            } => Some(New),
+            WindowEvent::MouseInput {
+                button: MouseButton::Right,
+                state: ElementState::Released,
+                ..
+            } => Some(Restart),
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(keycode),
+                        ..
+                    },
+                ..
+            } => match keycode {
+                Right => Some(Faster),
+                Left => Some(Slower),
+                Up => Some(VeryFaster),
+                Down => Some(VerySlower),
+                Z => Some(Prev),
+                X => Some(Next),
+                M => Some(Mutate),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+}
+
+struct State {
+    pub bytebeat: BytebeatState,
+    pub brainfuck: Brainfuck,
     pub mouse: MouseState,
-    pub input: Box<dyn Iterator<Item = i8>>,
-    pub frame: i64,
-    pub bytebeat_data: Box<[u8]>,
 }
 
 impl State {
     fn new() -> State {
-        let program = bf::random_bf(PROGRAM_LENGTH);
-        // let bytebeat = bytebeat::compile(
-        //     bytebeat::parse_beat("sx sy << my - my | mx % sx + sx / my & sy | t my - +")
-        //         .expect("bepis"),
-        // )
-        // .expect("conk");
-        let bytebeat = bytebeat::random_beat(10);
-        // let program = from_string("+[>+]");
-        println!("{}", bytebeat);
-
         State {
-            stack: Vec::with_capacity(10),
-            bytebeat,
+            bytebeat: BytebeatState::new(),
+            brainfuck: Brainfuck::new(),
+            mouse: MouseState::new(),
+        }
+    }
+}
+
+struct Brainfuck {
+    pub program: bf::Program,
+    pub state: bf::BFState,
+    pub speed: usize,
+    pub input: Box<dyn Iterator<Item = i8>>,
+}
+
+impl Brainfuck {
+    fn new() -> Brainfuck {
+        // let program = from_string("+[>+]");
+        let program = bf::random_bf(PROGRAM_LENGTH);
+        Brainfuck {
             program,
             state: bf::BFState::new(),
-            brainfuck_speed: INITIAL_SPEED,
-            mouse: MouseState::new(),
-            input: Box::new("".as_bytes().iter().cycle().map(|&b| b as i8)),
-            frame: 0,
-            bytebeat_speed: 1,
-            bytebeat_data: vec![0; BYTEBEAT_WIDTH * BYTEBEAT_HEIGHT].into_boxed_slice(),
+            speed: INITIAL_SPEED,
+            input: Box::new("Hello, world!".as_bytes().iter().cycle().map(|&b| b as i8)),
         }
+    }
+
+    fn handle_input(&mut self, control: Controls) {
+        use Controls::*;
+        match control {
+            New => {
+                self.program = bf::random_bf(PROGRAM_LENGTH);
+            }
+            Restart => (),
+            Next => unimplemented!(),
+            Prev => unimplemented!(),
+            Mutate => unimplemented!(),
+            VerySlower => self.speed /= 2,
+            Slower => self.speed = self.speed.saturating_sub(1),
+            Faster => self.speed += 1,
+            VeryFaster => self.speed = (self.speed * 2).max(2_000_000),
+        }
+
+        match control {
+            New | Restart | Next | Prev | Mutate => {
+                self.state = bf::BFState::new();
+                self.speed = 1;
+            }
+            _ => (),
+        }
+    }
+
+    fn render(&mut self, image: &mut Image) {
+        let instr = *self
+            .program
+            .instrs
+            .get(self.state.program_pointer)
+            .unwrap_or(&bf::BFChar::Plus);
+
+        render_bf(image, &self.state, instr);
+    }
+}
+
+struct BytebeatState {
+    pub stack: Vec<bytebeat::Val>,
+    pub bytebeats: Vec<bytebeat::Program>,
+    pub image_data: Box<[u8]>,
+    pub index: usize,
+    pub frame: i64,
+    pub speed: i64,
+}
+
+impl BytebeatState {
+    fn new() -> BytebeatState {
+        // let bytebeat = bytebeat::compile(
+        //     bytebeat::parse_beat("t sy my - sx mx - ^ mx - my + /").expect("bepis"),
+        // )
+        // .expect("conk");
+        let bytebeat = bytebeat::random_beat(PROGRAM_LENGTH);
+        println!("{}", bytebeat);
+
+        BytebeatState {
+            stack: Vec::with_capacity(PROGRAM_LENGTH),
+            bytebeats: vec![bytebeat],
+            image_data: vec![0; BYTEBEAT_WIDTH * BYTEBEAT_HEIGHT].into_boxed_slice(),
+            index: 0,
+            frame: 0,
+            speed: 1,
+        }
+    }
+
+    fn handle_input(&mut self, control: Controls) {
+        use Controls::*;
+        match control {
+            New => {
+                self.bytebeats.push(bytebeat::random_beat(PROGRAM_LENGTH));
+                self.index += 1;
+            }
+            Restart => (),
+            Next => self.index = (self.index + 1).min(self.bytebeats.len() - 1),
+            Prev => self.index = self.index.saturating_sub(1),
+            Mutate => unimplemented!(),
+            VerySlower => self.speed /= 2,
+            Slower => self.speed -= 1,
+            Faster => self.speed += 1,
+            VeryFaster => self.speed *= 2,
+        }
+
+        match control {
+            New | Restart | Next | Prev | Mutate => {
+                self.frame = 0;
+                self.speed = 1;
+                println!("{}", self.bytebeats[self.index]);
+            }
+            _ => (),
+        }
+    }
+
+    fn render(&mut self, image: &mut Image, mouse: &MouseState) {
+        let program = &self.bytebeats[self.index];
+        let t = self.frame;
+        self.image_data
+            .par_chunks_mut(BYTEBEAT_WIDTH)
+            .enumerate()
+            .for_each_init(
+                || Vec::with_capacity(32),
+                |stack, (screen_y, row)| {
+                    for screen_x in 0..BYTEBEAT_HEIGHT {
+                        row[screen_x] = bytebeat::eval_beat(
+                            stack,
+                            program,
+                            t,
+                            mouse.x as i64,
+                            mouse.y as i64,
+                            screen_x as i64,
+                            screen_y as i64,
+                        )
+                        .into();
+                    }
+                },
+            );
+
+        render_image(image, self.image_data.as_ref());
     }
 }
 
@@ -182,30 +288,6 @@ pub fn render_image(image: &mut Image, values: &[u8]) {
             let screen_x = x / width_scale_factor;
             let screen_y = y / height_scale_factor;
             let value = values[screen_y * BYTEBEAT_WIDTH + screen_x];
-            *pixel = Color {
-                r: 0,     //value.wrapping_mul(63),
-                g: value, //value.wrapping_mul(65),
-                b: 0,     //value.wrapping_mul(67),
-            };
-        }
-    }
-}
-
-pub fn render_bytebeat(image: &mut Image, state: &mut State) {
-    let width = image.width() as usize;
-    for (y, row) in image.chunks_mut(width).enumerate() {
-        for (x, pixel) in row.iter_mut().enumerate() {
-            let value: u8 = bytebeat::eval_beat(
-                &mut state.stack,
-                &state.bytebeat,
-                state.frame,
-                state.mouse.x as i64,
-                state.mouse.y as i64,
-                x as i64,
-                y as i64,
-            )
-            .into();
-
             *pixel = Color {
                 r: 0,     //value.wrapping_mul(63),
                 g: value, //value.wrapping_mul(65),
