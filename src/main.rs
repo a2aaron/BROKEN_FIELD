a3fb9b0b29d450c03e4e2827887a7f2f86794a37
Aaron Kofsky <aaronko@umich.edu>
2022-08-31 22:31:42 -0400

    Create an extremely useful webserver using warp
    :3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{hyper::Uri, Filter, Reply};
+
+#[tokio::main]
+async fn main() {
+    let filter = warp::path("BROKEN_FIELD")
+        .and(warp::path::param())
+        .and(warp::header::optional("user-agent"))
+        .map(|param: i32, user_agent: Option<String>| -> Box<dyn Reply> {
+            if param == 69 {
+                Box::new(warp::redirect(Uri::from_static(
+                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                )))
+            } else {
+                let html = include_str!("website.html");
+                let html = warp::reply::html(html);
+                Box::new(html)
+            }
+        });
+
+    warp::serve(filter).run(([127, 0, 0, 1], 3030)).await;
 }
