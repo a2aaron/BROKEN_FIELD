80f29d2fe393294239472b801b8d679cfc9f2a4e
Aaron Kofsky <aaronko@umich.edu>
2022-08-31 23:17:08 -0400

    HOMEPAGE_REDIRECT
    Redirect URLs without the ID to the BROKEN_FIELD homepage.
    
    Also, use temporary redirects for now--permenant redirects are annoying
    to test with since the browser will cache them.

--- url_shortener/src/main.rs
+++ url_shortener/src/main.rs
@@ -4,6 +4,12 @@ use warp::{hyper::Uri, Filter};
 
 #[tokio::main]
 async fn main() {
+    let home_page_redirect = warp::filters::method::get()
+        .and(warp::path("BROKEN_FIELD"))
+        .map(|| {
+            warp::redirect::temporary(Uri::from_static("https://a2aaron.github.io/BROKEN_FIELD/"))
+        });
+
     let redirect = warp::filters::method::get()
         .and(warp::path("BROKEN_FIELD"))
         .and(warp::path::param())
@@ -11,7 +17,8 @@ async fn main() {
             let query = id_to_query_params(id);
             let path = format!("https://a2aaron.github.io/BROKEN_FIELD/?{}", query);
             let uri = path.parse::<Uri>().unwrap();
-            warp::redirect(uri)
+            // Note: this should be a permenant redirect in the actual live site.
+            warp::redirect::temporary(uri)
         });
 
     let create = warp::filters::method::post()
@@ -19,11 +26,11 @@ async fn main() {
         .and(warp::filters::body::json())
         .map(|json: HashMap<String, String>| warp::reply());
 
-    warp::serve(redirect.or(create))
+    warp::serve(home_page_redirect.or(redirect).or(create))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
 
 fn id_to_query_params(id: String) -> String {
-    return "bytebeat=dA%3D%3D&color=ffb9d2".to_string();
+    return "bytebeat=dA%3D%3D&color=FFFFFF".to_string();
 }
