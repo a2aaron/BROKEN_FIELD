c75f48c4273008da8681843f0e5e3cc55cddf3e3
Aaron Kofsky <aaronko@umich.edu>
2022-08-31 22:54:49 -0400

    Start work making the URL shortner.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-use warp::{hyper::Uri, Filter, Reply};
+use std::collections::HashMap;
+
+use warp::{hyper::Uri, Filter};
 
 #[tokio::main]
 async fn main() {
-    let filter = warp::path("BROKEN_FIELD")
+    let redirect = warp::filters::method::get()
+        .and(warp::path("BROKEN_FIELD"))
         .and(warp::path::param())
-        .and(warp::header::optional("user-agent"))
-        .map(|param: i32, user_agent: Option<String>| -> Box<dyn Reply> {
-            if param == 69 {
-                Box::new(warp::redirect(Uri::from_static(
-                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-                )))
-            } else {
-                let html = include_str!("website.html");
-                let html = warp::reply::html(html);
-                Box::new(html)
-            }
+        .map(|id: String| {
+            let query = id_to_query_params(id);
+            let path = format!("https://a2aaron.github.io/BROKEN_FIELD/?{}", query);
+            let uri = path.parse::<Uri>().unwrap();
+            warp::redirect(uri)
         });
 
-    warp::serve(filter).run(([127, 0, 0, 1], 3030)).await;
+    let create = warp::filters::method::post()
+        .and(warp::path("BROKEN_FIELD"))
+        .and(warp::filters::body::json())
+        .map(|json: HashMap<String, String>| warp::reply());
+
+    warp::serve(redirect.or(create))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+fn id_to_query_params(id: String) -> String {
+    return "bytebeat=dA%3D%3D&color=ffb9d2".to_string();
 }
